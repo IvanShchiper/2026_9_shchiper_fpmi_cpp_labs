492c7e20f260ce8fd7ae04ad0dc2daf62addfac2
IvanShchiper <ivan.shchiper04162009@gmail.com>
2026-09-03 23:40:52 +0300

    Add files via upload

---
+++ semester_1/lab1_introduction/bonus/bonus/bonus.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n, k;
+
+    std::cout << "enter n,k\n";
+    std::cin >> n >> k;
+
+    for (int i = 1; i < n; i++) {
+        std::cout << "   ";
+
+    }
+    int x = 1;
+    for (int i = 0; i < k; i++) {
+        if(x<10)
+            std::cout << "  " << x;
+        else
+            std::cout << " " << x;
+        x++;
+        
+        if (n == 7) {
+            std::cout << std::endl;
+            n = 0;
+        }
+        n++;
+        
+    }
+    
+
+}
+
