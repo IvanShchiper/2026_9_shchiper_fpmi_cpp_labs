b76a6fbc880a89b6a09a8d5b3e7e07a346feac22
IvanShchiper <ivan.shchiper04162009@gmail.com>
2026-09-10 22:20:53 +0300

    Add files via upload

---
+++ semester_1/lab2_integer_arithmetics/lab2/lab2/lab2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+int main()
+{
+    int a, b;
+    std::cout << "enter a\n";
+    std::cin >> a;
+    std::cout << "enter b\n";
+    std::cin >> b;
+    int res = 3;
+    for (int n = 1; res <= b; n++) {
+        if (res >= a)
+            std::cout << res << std::endl;
+        res = pow(2, pow(2, n)) + 1;
+    }
+    
+    return 0;
+}
